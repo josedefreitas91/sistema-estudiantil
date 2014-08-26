f0e815637dc75c35024389bb14f4ff2ff87f897b
josedefreitas91@gmail.com <josedefreitas91@gmail.com@ba32b583-69c8-c8cb-628f-e5069c855551>
2014-08-26 19:55:27 +0000

    cambios

--- trunk/app/Controller/SeaController.php
+++ trunk/app/Controller/SeaController.php
@@ -1,9 +1,10 @@
 <?php
 
 class SeaController extends AppController {
-    var $name = 'Sea';
+    //var $name = 'Sea';
     public $helpers = array('Html', 'Form');
     public $uses = array('Usuario');
+    //public $components = array('Session', 'RequestHandler');
     
     public function index() {
         $this->set('title_for_layout', 'Sea - La Victoria');
@@ -24,14 +25,17 @@ class SeaController extends AppController {
         $datos['Usuario']['nombres'] = ucwords($this->request->data['Usuario']['nombres']);
         $datos['Usuario']['apellidos'] = ucwords($this->request->data['Usuario']['apellidos']);
         $datos['Usuario']['pregunta_s'] = ucwords($this->request->data['Usuario']['pregunta_s']);
-        //$datos['Usuario']['online'] = 0;
-        //$datos['Usuario']['nivel'] = 1;
-        //$datos['Usuario']['estatus'] = 1;
+        $datos['Usuario']['online'] = 0;
+        $datos['Usuario']['nivel'] = 1;
+        $datos['Usuario']['estatus'] = 1;
         //pr($datos);
         //die();
         if ($this->Usuario->save($datos)){
             $this->Session->setFlash('Registro Guardado!');
             $this->redirect(array('action' => 'index'));
+        }else{
+            $this->Session->setFlash('No se puede guardar');
+            //$this->redirect(array('action' => 'index'));
         }
         $this->autoRender=false;
     }

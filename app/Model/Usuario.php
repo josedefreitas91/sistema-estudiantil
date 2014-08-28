a0eb12fe14bf35e4054c4ef59b30d5f27ac3281f
josedefreitas91@gmail.com <josedefreitas91@gmail.com@ba32b583-69c8-c8cb-628f-e5069c855551>
2014-08-28 02:09:19 +0000

    Agregada validaciones en el modelo, y cambios en el controlador y la vista

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -12,9 +12,21 @@ class Usuario extends Model {
                 'rule' => 'notEmpty',
                 'required' => true,
                 'message' => 'Este campo no puede estar vacio!',
+            ),
+            'min' => array(
+                'rule'    => array('minLength', '8'),
+                'message' => 'Debe contener minimo 8 caracteres'
+            ),
+            'Unico' => array(
+                'rule' => 'isUnique',
+                'required' => true,
+                'allowEmpty' => false,
+                'on' => 'create', // here
+                'last' => false,
+                'message' => 'Nombre de usuario ya existe!'
             )
         ),
-       /* 'password' => array(
+        'password' => array(
             'alphaNumeric' => array(
                 'rule'     => 'alphaNumeric',
                 'required' => true,
@@ -23,7 +35,7 @@ class Usuario extends Model {
             'rule'    => array('minLength', '8'),
             'message' => 'Debe contener minimo 8 caracteres'
         ),
-      */  /*'email' => array(
+        /*'email' => array(
             'notEmpty' => array(
                 'rule' => 'notEmpty',
                 'required' => true,
@@ -39,6 +51,10 @@ class Usuario extends Model {
                 'rule' => 'notEmpty',
                 'required' => true,
                 'message' => 'Este campo no puede estar vacio!',
+            ),
+            'min' => array(
+                'rule'    => array('minLength', '3'),
+                'message' => 'El nombre debe contener minimo 3 caracteres'
             )
         ),
         'apellidos' => array(
@@ -46,6 +62,10 @@ class Usuario extends Model {
                 'rule' => 'notEmpty',
                 'required' => true,
                 'message' => 'Este campo no puede estar vacio!',
+            ),
+            'min' => array(
+                'rule'    => array('minLength', '3'),
+                'message' => 'El nombre debe contener minimo 3 caracteres'
             )
         ),
         'pregunta_s' => array(
